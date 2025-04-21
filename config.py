ed486f3eb3ad4521ddee18b982f2da735cd518e3
osamacfc <osama1415malki@gmail.com>
2025-04-21 18:06:49 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 
-BOT_TOKEN = "ضع هنا رمز التوكن الخاص بالبوت"
+BOT_TOKEN = "7800594975:AAFz7imbaTGAYvf-WhKxgIxBxrgpKEiIsVo"
 ADMIN_ID = 585555633  # ID الخاص بك كمشرف
 ALL_USERS_CHAT_IDS = [585555633]  # يمكن تحديثها لاحقًا بمستخدمين فعليين
 TIMEZONE = "Asia/Riyadh"
