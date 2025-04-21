f3cf20feedeb8f6dab3834ddf66607d5e10efc50
osamacfc <osama1415malki@gmail.com>
2025-04-21 20:00:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,13 @@
 # config.py
 
 BOT_TOKEN = "7800594975:AAFz7imbaTGAYvf-WhKxgIxBxrgpKEiIsVo"
-ADMIN_ID = 585555633  # ID الخاص بك كمشرف
-ALL_USERS_CHAT_IDS = [585555633]  # يمكن تحديثها لاحقًا بمستخدمين فعليين
-TIMEZONE = "Asia/Riyadh"
+ADMIN_ID = 585555633
+
+# هذه تتحدث تلقائيًا من main عند تسجيل المستخدم
+ALL_USERS_CHAT_IDS = []
+
+# المسار إلى قاعدة البيانات
 USERS_DB_PATH = "users.json"
+
+# المنطقة الزمنية
+TIMEZONE = "Asia/Riyadh"
