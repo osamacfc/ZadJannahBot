484492ceecc43818d474de8ece2277e19d41c94c
osamacfc <osama1415malki@gmail.com>
2025-04-23 12:28:31 +0300

    Create daily_tasks.py

---
+++ daily_tasks.py
@@ -0,0 +1,28 @@
+# daily_tasks.py
+
+def send_morning_azkar(user_id):
+    pass
+
+def send_evening_azkar(user_id):
+    pass
+
+def send_witr_reminder():
+    pass
+
+def send_sleep_azkar():
+    pass
+
+def send_azkar_after_prayer(user_id):
+    pass
+
+def send_witr_dua():
+    pass
+
+def send_duha_reminder():
+    pass
+
+def send_midnight_istighfar():
+    pass
+
+def send_last_third_night():
+    pass
