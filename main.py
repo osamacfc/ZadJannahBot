942a79c88fefe011fb2a4c90da4a5216b5de7760
osamacfc <osama1415malki@gmail.com>
2025-04-24 23:30:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -734,7 +734,7 @@ def send_witr_message(message):
 def handle_witr_dua(call):
     send_witr_dua(call.message.chat.id)
     
-    if __name__ == "__main__":
-        bot.remove_webhook()
-        bot.set_webhook(url="https://zadjannahbot.onrender.com/")
-        app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+ if __name__ == "__main__":
+    bot.remove_webhook()
+    bot.set_webhook(url="https://zadjannahbot.onrender.com/")
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
